66ca4528346dcc99af603f1cec769ce2d95876b1
AngelinaRS <candy26y_@hotmail.com>
2015-10-05 11:11:17 -0600

    whit class

--- unit_test.py
+++ unit_test.py
@@ -5,14 +5,17 @@ import unittest
 class testCountriesAndCapitals(unittest.TestCase):
 
 	def test_is_alpha(self):
-		self.assertEqual(application.is_alpha("Guatemala"), True)
-		self.assertEqual(application.is_alpha("5"), False)
+		cc = application.CountriesAndCapitals()
+		self.assertEqual(cc.is_alpha("Guatemala"), True)
+		self.assertEqual(cc.is_alpha("5"), False)
 
 	def test_is_title(self):
-		self.assertEqual(application.is_title("guate"), "Guate")
+		cc = application.CountriesAndCapitals()
+		self.assertEqual(cc.is_title("guate"), "Guate")
 
 	def test_minuscule(self):
-		self.assertEqual(application.minuscule("GUATE"), "guate")
+		cc = application.CountriesAndCapitals()
+		self.assertEqual(cc.minuscule("GUATE"), "guate")
 
 if __name__ == '__main__':
 	unittest.main()
